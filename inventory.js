8d7dbcbe6fb1ae83de43d4bae13ff04f985c7694
idriss rami <rdriss306@gmail.com>
2024-12-26 17:27:25 +0100

    version finale

--- inventory.js
+++ inventory.js
@@ -1,78 +1,85 @@
-const product=require("./product")
-const prompt=require("prompt-sync")()
-const fs =require("fs")
-let prod=[]
+const product = require("./product")
+const prompt = require("prompt-sync")()
+const fs = require("fs")
+let prod = JSON.parse(fs.readFileSync("fa.json", "utf8"))
+ 
+
 class invertory {
-    
-    ajouter(){
-        let produit=new product()
-        produit.name=prompt("enter le name:")
-        if(!produit.name){
-       console.log("entre name de produits")
-       return
+
+    ajouter() {
+        let produit = new product();
+        produit.name = prompt("enter le name:") //declarer variable produit.name pour que l utilisateur ystoki lina l valeur
+        if (!produit.name) {
+            console.log("RESSAYER name de produits")
+            
         }
-        produit.description=prompt("entre le description :")
-        produit.quantity=Number(prompt("enter le quantiter :"))
-        produit.price=Number(prompt("enter le price :"))
+        produit.description = prompt("entre le description :")
+        produit.quantity = Number(prompt("enter le quantiter :"))
+        produit.price = Number(prompt("enter le price :"))
         prod.push(produit)
         //console.log(prod)
-        fs.writeFileSync("crud.json", JSON.stringify(prod, null, 2));
+        fs.writeFileSync("fa.json", JSON.stringify(prod, null, 2));
 
     }
-afficher(){
-    const da=fs.readFileSync("crud","utf8")
-    const donner=JSON.parse(da)
-const data=donner.map((item,index)=>{
-    console.log(`${index+1} name :${item.name} \n description :${item.description} \n quantity :${item.quantity} \nprice :${item.price}`)
-})
-console.log(data)
-}
-suprrimer(){
-    let produit=new product()
-    produit.id=prompt("donner le valeur id peur suprimer")  
-    if(produit.id >=0){
-        prod.splice(produit.id-1,1)
-        fs.writeFileSync("crud.json",JSON.stringify(prod))
+    afficher() {
+        const da = fs.readFileSync("fa.json", "utf8")
+        const donner = JSON.parse(da)
+        donner.map((item, index) => {
+            
+            console.log(`\nPRODUCT NUMBER ${index + 1}\n name :${item.name} \n description :${item.description} \n quantity :${item.quantity} \n price :${item.price}`)
+            
+            // console.log("product number: ", index + 1)
+            // console.log("product name: ", item.name)
+            // console.log("product description: ", item.description)
+            // console.log("product quantity: ", item.quantity)
+            // console.log("product price: ", item.price)
+        })
+    }
+    suprrimer() {
+        
+        let produitSupp = parseInt(prompt("entrer produit a supprimer: "));
+        if (produitSupp >= 0) {
+            prod.splice(produitSupp - 1, 1);
+            fs.writeFileSync("fa.json", JSON.stringify(prod));
+        }
+    }
+    modifier() {
+        
+        let id = prompt("donner la valeur id pour modifier:")
+        if (id >= 0) {
+            prod[id - 1].name = prompt("enter nouveau nom: ");
+            prod[id - 1].description = prompt("enter nouveau description: ");
+            prod[id - 1].price = Number(prompt("enter le price: "));
+            prod[id - 1].quantity = Number(prompt("enter le Qunatite: "));
+            fs.writeFileSync("fa.json", JSON.stringify(prod, null, 4));
+        }
     }
 }
-modifier(){
-    let produit=new product()
-    produit.id=prompt("donner le valeur id pour modifier:")  
-    if(produit.id>=0){
-prod[produit.id-1][" produit.name"]
-prod[produit.id-1].description
-prod[produit.id-1].name=prompt("enter le name")
-prod[produit.id-1].price=prompt("enter le name")
-fs.writeFileSync("crud.json",JSON.stringify(prod))
-    }
-}
-}
-const Tn=new invertory()
-do{
-    console.log("1=Ajouter un produit")
+const Tn = new invertory();
+do {
+    console.log("\n1=Ajouter un produit")
     console.log("2=Afficher tous les produits.")
-    console.log("3=Mettre à jour un produit.")
-    console.log("4=Supprimer un produit.")
+    console.log("3=supprimer un produit.")
+    console.log("4=Modifier un produit.")
     console.log("5=quitter")
-  const choix=prompt("enter le choix avec le menu ");
-  switch(choix){
-    case"1":
-       console.log(Tn.ajouter())
-       break;
-       case"2":
-       console.log(Tn.afficher())
-       break;
-       case"3":
-       console.log(Tn.deletP())
-       break;
-     case "4":
-        console.log(Tn.modifier())
-        break;
-    case"5":
-       return
-       break
-    default:
-        console.log("entree le nunber avec le choix")
-    
-  }
-}while(true)
+    const choix = prompt("enter le choix avec le menu ");
+    switch (choix) {
+        case "1":
+            Tn.ajouter()
+            break;
+        case "2":
+            Tn.afficher()
+            break;
+        case "3":
+            Tn.suprrimer()
+            break;
+        case "4":
+            Tn.modifier()
+            break;
+        case "5":
+            return;
+        default:
+            console.log("entree le nunber avec le choix")
+
+    }
+} while (true)
--- product.js
+++ product.js
@@ -1,50 +1,12 @@
-class product {
-    constructor(name,description,quantité,price)
-    this.name = name ;
-    this.description = description ;
-    this.quantité = quantité ;
-    this.price = price ;
-//         const { stringify } = require("querystring")
-const { json } = require("stream/consumers")
-
-class personne{
-    constructor (name,descriprtion);
-    this.name=name  
-    y
-
-    const name ('driss');
-    conste age 25;
-    const istudent(true);
- console.log(my name is ${name}i am $(24) old are yuou,i am a ${istudent});
-
-
- const day = "sunday";
-
-switch (day){
-    case "monday":
-        console.log("start of the week");
-        break;
-    case "friday":
-        console.log("end of the week");
-        break;
-    case "sunday":
-    case "saturday":
-        console.log("the weekend");
+class Products{
+    constructor(id,name,description,quantity,price){
+        this.id=id
+        this.name=name
+        this.description=description
+        this.quantity=quantity
+        this.price=price
+    }
 }
+module.exports=Products
 
-const day="sunday";
-switch(day){
-    case"monday";
-    consol.log(start of the week)
-    break;
-    case"friday";
-    consol.log("end of the week");
-    break;
-    case"sunday"
-    case "srturday
-    "
-consol.loge("the weekend");
-break;
 
-}
-let 
